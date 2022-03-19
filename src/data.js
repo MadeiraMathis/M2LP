fdd1cdca54817bd658393ab2a75ea19b79789f9e
unknown <mathis.madeira.com>
2022-03-19 15:51:56 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://www.kindpng.com/picc/m/495-4955824_sao-hollow-realization-kirito-render-by-thegothamguardian-kirito.png",
+        title: "KIRITO",
+        desc: "BG SUPREME",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/originals/1b/a0/f9/1ba0f9f0ecb7be8e3b03b294e2783b1d.png",
+        title: "TANJIRO",
+        desc: "BG SUPREME",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/originals/83/aa/85/83aa8535a3ea18b01d392fa73cbbd9c7.jpg",
+        title: "NARUTO",
+        desc: "BG SUPREME",
+        bg: "fbf0f4",
+    }
+]
